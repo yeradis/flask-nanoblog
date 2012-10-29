ac076a0173e06647923c2f9421c5cd9ae0d18782
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2012-10-29 22:00:05 +0400

    added "json" output

--- nanoblog/views.py
+++ nanoblog/views.py
@@ -11,9 +11,15 @@ class ListView(MethodView):
         posts = Post.objects.all()
         return render_template('posts/list.html', posts=posts)
 
+class JsonView(MethodView):
+    def get(self):
+        posts = Post.objects.all()
+        return render_template('posts/list.json', posts=posts)
+
 def register_blueprints(app):
     # Prevents circular imports
     app.register_blueprint(posts)
 
 # Register the urls
 posts.add_url_rule('/', view_func=ListView.as_view('list'))
+posts.add_url_rule('/json', view_func=JsonView.as_view('json'))
